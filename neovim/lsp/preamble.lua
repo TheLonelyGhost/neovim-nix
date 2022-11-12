860d26a93077b8acd8e9627bdfeeea2094ddf97a
David Alexander <opensource@thelonelyghost.com>
2022-11-12 18:57:39 -0500

    Include cmp-nvim-lsp in the lsp startup code

--- neovim/lsp/preamble.lua
+++ neovim/lsp/preamble.lua
@@ -1,5 +1,12 @@
 local lspconfig = require('lspconfig')
+
 local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
 
 -- Mappings
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
