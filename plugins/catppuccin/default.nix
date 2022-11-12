5abc31aef5899a9b74a8a5abff6c226db1598bea
David Alexander <opensource@thelonelyghost.com>
2022-11-12 18:55:45 -0500

    Adds vim plugin for catppuccin

---
+++ plugins/catppuccin/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+let
+  version = "0.2.6";
+in
+pkgs.vimUtils.buildVimPluginFrom2Nix {
+  pname = "catppuccin-nvim";
+  inherit version;
+
+  src = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "nvim";
+    rev = "v${version}";
+    sha256 = "sha256-vhKhEXCkmz0KH8LlZzcOvGkvdwp7TtI7ZV7ZxojgnAI="; # pkgs.lib.fakeSha256;
+  };
+
+  meta = {
+    description = "(Neovim plugin) Soothing pastel theme for the high-spirited!";
+    homepage = "https://catppuccin.com/";
+    license = pkgs.lib.licenses.mit;
+  };
+}
